e09fb29aed5e34b9a95cbaef9a2434b0c96a04ce
Dariusz Socha <dariusz.socha@genesys.com>
2020-10-01 07:52:44 +0200

    create-react-library

---
+++ gc-region-picker/example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'gc-region-picker'
+import 'gc-region-picker/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
