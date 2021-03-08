4fde14e8c976bf16fbadae4104188c0df6d3d7af
Dariusz Socha <dariusz.socha@genesys.com>
2021-03-08 11:31:37 +0100

    styling in the testing app

--- example/src/App.js
+++ example/src/App.js
@@ -3,7 +3,15 @@ import React from 'react';
 import { GcRegionPicker } from 'gc-region-picker';
 
 const App = () => {
-  return <GcRegionPicker />;
+  return (
+    <div
+      style={{ display: 'flex', justifyContent: 'center', marginTop: '100px' }}
+    >
+      <div style={{ width: '300px' }}>
+        <GcRegionPicker />
+      </div>
+    </div>
+  );
 };
 
 export default App;
