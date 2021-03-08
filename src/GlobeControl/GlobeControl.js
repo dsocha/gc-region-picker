6150d667928d92160f339d16bc9edaf26dff0181
Dariusz Socha <dariusz.socha@genesys.com>
2021-03-08 18:27:14 +0100

    minor improvements

--- src/GlobeControl/GlobeControl.js
+++ src/GlobeControl/GlobeControl.js
@@ -6,6 +6,8 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { globeTexture } from './GlobeTexture';
 
+const GLOBE_SIZE = 48;
+
 const GlobeControl = (props) => {
   const canvasRef = useRef(null);
 
@@ -35,29 +37,34 @@ const GlobeControl = (props) => {
   };
 
   const loadScene = () => {
+    // <scene>
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera(75, canvasRef.current.clientWidth / canvasRef.current.clientHeight, 0.1, 1000);
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(canvasRef.current.clientWidth, canvasRef.current.clientHeight);
     canvasRef.current.appendChild(renderer.domElement);
+    // </scene>
 
+    // <globe>
     loader = new THREE.TextureLoader();
-
     const tex = loader.load(globeTexture);
-
     globe = new THREE.Mesh(
-      new THREE.SphereGeometry(48, 96, 96),
+      new THREE.SphereGeometry(GLOBE_SIZE, 96, 96),
       new THREE.MeshPhongMaterial({
         map: tex,
         bumpScale: 0.005,
         specular: new THREE.Color('grey')
       })
     );
-
     scene.add(globe);
+    // </globe>
+
+    // <light>
     light = new THREE.HemisphereLight(0xffffbb, 0x080820, 1.3);
     scene.add(light);
+    // </light>
 
+    // <controls>
     controls = new OrbitControls(camera, renderer.domElement);
     controls.minDistance = 80;
     controls.maxDistance = 170;
@@ -67,14 +74,15 @@ const GlobeControl = (props) => {
     controls.autoRotateSpeed = 0.5;
     camera.position.set(0, 50, 80);
     controls.update();
-
-    var animate = function () {
-      requestAnimationFrame(animate);
-      controls.update();
-      renderer.render(scene, camera);
-    };
+    // </controls>
 
     animate();
+  };
+
+  const animate = function () {
+    requestAnimationFrame(animate);
+    controls.update();
+    renderer.render(scene, camera);
   };
 
   return (
